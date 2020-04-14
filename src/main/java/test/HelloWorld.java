87ad1a4384acd5b1dad2951dc354e2061ce983ac
Florek <pawel.florczak87@gmail.com>
2020-04-14 23:17:51 +0200

    Add new attributes to configuration

--- src/main/java/test/HelloWorld.java
+++ src/main/java/test/HelloWorld.java
@@ -5,12 +5,22 @@ import org.springframework.stereotype.Service;
 @Service("helloWorldService")
 public class HelloWorld {
     private String name;
-    
-    public String getName() {
-        return name;
-    }
+    private int age;
+    private String address;
     
     public void setName(String name) {
         this.name = name;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public void sayHello() {
+        System.out.println(name + " " + address + " " + age);
     }
 }
--- src/main/java/test/Start.java
+++ src/main/java/test/Start.java
@@ -5,12 +5,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Start {
 
+    private static ApplicationContext context;
+
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        context = new ClassPathXmlApplicationContext("applicationContext.xml");
         
         HelloWorld helloWorldService = (HelloWorld) context.getBean("helloWorldService");
 
-        System.out.println(helloWorldService.getName());
+        helloWorldService.sayHello();
     }
 
 }
